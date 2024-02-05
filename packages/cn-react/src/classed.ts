e4902dbf6e6eff83bd3fa38a1a6ccc40b19282de
maroqijalil <jalilpro31@gmail.com>
2024-02-05 09:22:19 +0700

    refactor: change classed

--- packages/cn-react/src/classed.ts
+++ packages/cn-react/src/classed.ts
@@ -1,16 +1,19 @@
-import type { ClassValue } from 'clsx';
 import {
   ComponentProps as BaseComponentProps,
   ComponentType as BaseComponentType,
+  ForwardRefExoticComponent,
   ReactHTML,
+  RefAttributes,
   createElement,
   forwardRef,
   useMemo,
 } from 'react';
 import type {
-  Classed,
+  ClassValue,
   ClassedProps,
+  ClassedPropsParamFunction,
   ClassedPropsParamType,
+  ClassedPropsRef,
   ComponentRefType,
   PropsWithClassed,
 } from './types';
@@ -22,14 +25,50 @@ import {
   hasClassedSign,
 } from './utils';
 
-export const classed: Classed = <
+export function classed<
+  Props extends object & ClassedProps,
+  ComponentType extends keyof ReactHTML | BaseComponentType = keyof ReactHTML | BaseComponentType,
+>(
+  component: ComponentType,
+  passedProps?: ClassedPropsParamFunction<BaseComponentProps<ComponentType> & Props>,
+): ForwardRefExoticComponent<
+  ClassedPropsRef<ComponentType, Props> & RefAttributes<ComponentRefType<ComponentType, Props>>
+>;
+
+export function classed<Props extends object & ClassedProps>(
+  component: BaseComponentType<Props>,
+  passedProps?: ClassedPropsParamFunction<BaseComponentProps<BaseComponentType<Props>> & Props>,
+): ForwardRefExoticComponent<
+  ClassedPropsRef<BaseComponentType<Props>, Props> &
+    RefAttributes<ComponentRefType<BaseComponentType, Props>>
+>;
+
+export function classed<
+  Props extends object & ClassedProps,
+  ComponentType extends keyof ReactHTML | BaseComponentType = keyof ReactHTML | BaseComponentType,
+>(
+  component: ComponentType,
+  ...passedProps: ClassValue[]
+): ForwardRefExoticComponent<
+  ClassedPropsRef<ComponentType, Props> & RefAttributes<ComponentRefType<ComponentType, Props>>
+>;
+
+export function classed<Props extends object & ClassedProps>(
+  component: BaseComponentType<Props>,
+  ...passedProps: ClassValue[]
+): ForwardRefExoticComponent<
+  ClassedPropsRef<BaseComponentType<Props>, Props> &
+    RefAttributes<ComponentRefType<BaseComponentType, Props>>
+>;
+
+export function classed<
   Props extends object & ClassedProps,
   ComponentType extends keyof ReactHTML | BaseComponentType = keyof ReactHTML | BaseComponentType,
 >(
   component: ComponentType,
   passedProps?: ClassedPropsParamType<BaseComponentProps<ComponentType> & Props>,
   ...classes: ClassValue[]
-) => {
+) {
   type TargetProps = BaseComponentProps<ComponentType> & Props;
 
   const Component = forwardRef<
@@ -61,4 +100,4 @@ export const classed: Classed = <
   });
 
   return Component;
-};
+}
--- packages/cn-react/src/types.ts
+++ packages/cn-react/src/types.ts
@@ -1,72 +1,50 @@
-import type { ClassValue } from 'clsx';
+import type { ClassValue as BaseClassValue, ClassDictionary } from 'clsx';
 import type {
   ComponentProps as BaseComponentProps,
   ComponentType as BaseComponentType,
   PropsWithoutRef as BasePropsWithoutRef,
   ClassAttributes,
   ElementRef,
-  ForwardRefExoticComponent,
+  PropsWithChildren,
   ReactHTML,
-  RefAttributes,
 } from 'react';
 
 export type ExtractHTMLElementByProps<PropsType, FallbackType = never> =
   PropsType extends ClassAttributes<infer ElementType> ? ElementType : FallbackType;
 
+export type ClassValue =
+  | (ClassValue | ClassDictionary)[]
+  | string
+  | number
+  | null
+  | boolean
+  | undefined;
+
 export type ClassedProps = { className?: string };
 
-export type PropsWithClassed<Props> = Omit<Props, 'className'> & { className?: ClassValue };
+export type PropsWithClassed<Props> = Omit<Props, 'className'> & { className?: BaseClassValue };
 
-export type ClassedPropsParamFunction<Props> = (props: PropsWithClassed<Props>) => ClassValue;
+export type ClassedPropsParamFunction<Props> = (
+  props: PropsWithClassed<Props>,
+) => BaseClassValue | void;
 
 export type ClassedPropsParamType<Props> = ClassValue | ClassedPropsParamFunction<Props>;
 
-export type ComponentRefType<ComponentType extends keyof ReactHTML | BaseComponentType, Props> =
+export type ComponentRefType<
+  ComponentType extends keyof ReactHTML | BaseComponentType | HTMLElement,
+  Props,
+> =
   ExtractHTMLElementByProps<Props> extends never
-    ? ElementRef<ComponentType> extends never
-      ? BaseComponentType<Props>
-      : ElementRef<ComponentType>
+    ? ComponentType extends keyof ReactHTML | BaseComponentType
+      ? ElementRef<ComponentType> extends never
+        ? BaseComponentType<Props>
+        : ElementRef<ComponentType>
+      : ComponentType
     : ExtractHTMLElementByProps<Props>;
 
-export type PropsWithoutRef<
+export type ClassedPropsRef<
   ComponentType extends keyof ReactHTML | BaseComponentType<any>,
   Props,
-> = PropsWithClassed<BasePropsWithoutRef<BaseComponentProps<ComponentType> & Props>>;
-
-export type Classed = {
-  <
-    Props extends object & ClassedProps,
-    ComponentType extends keyof ReactHTML | BaseComponentType = keyof ReactHTML | BaseComponentType,
-  >(
-    component: ComponentType,
-    passedProps?: ClassedPropsParamFunction<BaseComponentProps<ComponentType> & Props>,
-  ): ForwardRefExoticComponent<
-    PropsWithoutRef<ComponentType, Props> & RefAttributes<ComponentRefType<ComponentType, Props>>
-  >;
-
-  <Props extends object & ClassedProps>(
-    component: BaseComponentType<Props>,
-    passedProps?: ClassedPropsParamFunction<BaseComponentProps<BaseComponentType<Props>> & Props>,
-  ): ForwardRefExoticComponent<
-    PropsWithoutRef<BaseComponentType<Props>, Props> &
-      RefAttributes<ComponentRefType<BaseComponentType, Props>>
-  >;
-
-  <
-    Props extends object & ClassedProps,
-    ComponentType extends keyof ReactHTML | BaseComponentType = keyof ReactHTML | BaseComponentType,
-  >(
-    component: ComponentType,
-    ...passedProps: ClassValue[]
-  ): ForwardRefExoticComponent<
-    PropsWithoutRef<ComponentType, Props> & RefAttributes<ComponentRefType<ComponentType, Props>>
-  >;
-
-  <Props extends object & ClassedProps>(
-    component: BaseComponentType<Props>,
-    ...passedProps: ClassValue[]
-  ): ForwardRefExoticComponent<
-    PropsWithoutRef<BaseComponentType<Props>, Props> &
-      RefAttributes<ComponentRefType<BaseComponentType, Props>>
-  >;
-};
+> = PropsWithChildren<
+  PropsWithClassed<BasePropsWithoutRef<BaseComponentProps<ComponentType> & Props>>
+>;
