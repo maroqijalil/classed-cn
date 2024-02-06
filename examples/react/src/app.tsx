768d060cf3cd4ace5178933e04153453dbe6f237
maroqijalil <jalilpro31@gmail.com>
2024-02-06 22:12:56 +0700

    refactor: change setting and type

--- examples/react/src/app.tsx
+++ examples/react/src/app.tsx
@@ -1,6 +1,6 @@
 import { classed } from '@classed-component/cn-react';
 
-const Component = classed('div');
+const Component = classed('div', 'h-full w-full', 'flex justify-center');
 
 export default function App() {
   return <Component>React</Component>;
